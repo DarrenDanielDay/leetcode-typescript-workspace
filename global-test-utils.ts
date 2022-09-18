ba406026a410897f0e619d8f0d7797d1fafba39b
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-09-19 00:36:13 +0800

    fix: use `_.isEqual` to compare

--- global-test-utils.ts
+++ global-test-utils.ts
@@ -448,7 +448,7 @@ const testObject = (() => {
             if (typeof output === "number" && typeof expected === "number") {
               return Math.abs(output - expected) <= 1e-5;
             }
-            return _.eq(output, expected);
+            return _.isEqual(output, expected);
           };
           runTestAndCompare(input, tester, () => Reflect.apply(solution, void 0, input), "case", i);
         } catch (error) {
